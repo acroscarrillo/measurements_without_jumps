01e5bb19c0a53a128478241e1384057663894b24
Alex's mac <a.croscarrillo@gmail.com>
2024-04-17 18:18:12 +0100

    paper figs completed

--- data_gen/fail_probability_vs_Delta_params_1.jl
+++ data_gen/fail_probability_vs_Delta_params_1.jl
@@ -10,7 +10,7 @@ using LaTeXStrings
 #########################
 # Simulation parameters #
 #########################
-N_tot_runs = 1000
+N_tot_runs = 10000
 
 N = 100
 Δ_array = Vector(range(20,200,length=10))
@@ -41,8 +41,8 @@ n = real.([ψ_1_0'*σ_1*ψ_1_0 ψ_1_0'*σ_2*ψ_1_0 ψ_1_0'*σ_3*ψ_1_0])
 #################
 # Initial state #
 #################
-final_fidelity = zeros(N_tot_runs,length(Δ_array))
-pbar = ProgressBar(total=length(final_fidelity))
+final_fidelity_array = zeros(N_tot_runs,length(Δ_array))
+pbar = ProgressBar(total=length(final_fidelity_array))
 for (k,Δ) in enumerate(Δ_array)
     for m=1:N_tot_runs
         ψ_t = ψ_0
@@ -62,18 +62,21 @@ for (k,Δ) in enumerate(Δ_array)
 
             end
         end
-        final_fidelity[m,k] = norm(ψ_t'*ψ_0)^2    
+        final_fidelity_array[m,k] = norm(ψ_t'*ψ_0)^2   
+        update(pbar)
     end
-    update(pbar)
 end
 
 P_threshold = 0.99
 success_array =  zeros(length(Δ_array))
 for (k,Δ) in enumerate(Δ_array)
-    temp = final_fidelity[:,k] 
+    temp = final_fidelity_array[:,k] 
     success_array[k] = length(temp[ temp .> P_threshold ])/ N_tot_runs
 end
-scatter(Δ_array,success_array)
+
+
+width_px= 340.39020340390204  # or  width_inches*DPI
+scatter(Δ_array,success_array,size = (width_px,width_px*0.6),legend=false,ylabel=L"\textrm{Success \ \ probability}",xlab=L"\Delta/\chi",xtickfontsize=8,ytickfontsize=8,guidefont=font(10),dpi=600,widen=false,tickdirection=:out,right_margin = 2Plots.mm,bottom_margin = 0Plots.mm,fontfamilys = "Times New Roman",legendfontsize=9,ms=3,xlim=(-10 + Δ_array[1],10 + Δ_array[end]),ylim=(0.6,1.05),title=L"N = "*"$N,   "*L"N_t="*"$Nt",alpha=0.75,titlefontsize=10)
 
 # savefig("figs/success_prob_vs_Delta.png")
 # savefig("figs/success_prob_vs_Delta.pdf")
